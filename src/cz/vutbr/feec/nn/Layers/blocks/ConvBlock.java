a38adf21b10ac8846bac199b181127686ada166b
xbitz75 <xbitz75@gmail.com>
2019-04-28 12:53:17 +0200

    test 4 version

--- src/cz/vutbr/feec/nn/Layers/blocks/ConvBlock.java
+++ src/cz/vutbr/feec/nn/Layers/blocks/ConvBlock.java
@@ -8,19 +8,15 @@ import cz.vutbr.feec.nn.NetworkGenerator;
 public class ConvBlock extends AbstractLayer {
 
 	private int filters;
-	private float droupoutRate;
-	private int poolSize = 2;
 
 	private enum ACTIVATION {
 		relu, tanh, sigmoid
 	}
 
-	private String kernelSize = "(3, 3)";
 	private ACTIVATION activation;
 
 	public ConvBlock(int id, NetworkGenerator network, int filters) {
 		super(id, network);
-		this.droupoutRate = (float) (Math.random() / 2); // randomly generated rate parameter for dropout
 		this.filters = filters;
 		layerType = "ConvBlock";
 		createConnections();
@@ -28,17 +24,17 @@ public class ConvBlock extends AbstractLayer {
 		shape0 = shape0 - (6 - 2); // TODO make kernelSize a variable
 		shape1 = shape1 - (6 - 2); // TODO (kernelSize - 1)
 		shape2 = this.filters;
+		int poolSize = 2;
 		shape0 = shape0 / poolSize;
 		shape1 = shape1 / poolSize;
 	}
 
 	@Override
 	public String build() {
-		return "conv_block01 = Conv2D(" + filters + ", " + kernelSize + ", padding=\"same\", activation=\"" + activation
-				+ "\")(" + getPreviousLayers()[0].getLayerId() + ")\n" + "conv_block02 = Conv2D(" + filters + ", "
-				+ kernelSize + ", activation=\"" + activation + "\", padding=\"same\")(conv_block01)\n"
-				+ "conv_block03 = MaxPooling2D(pool_size=(2,2))(conv_block02)\n" + "layer_" + String.format("%03d", id)
-				+ " = Dropout(" + droupoutRate + ")(conv_block03) # last Conv block";
+		String kernelSize = "(3, 3)";
+		return  "conv_block01 = Conv2D(" + filters + ", " + kernelSize + ", padding=\"same\", activation=\"" + activation
+				+ "\")(" + getPreviousLayers()[0].getLayerId() + ")\n" +
+				"layer_" + String.format("%03d", id) + " = BatchNormalization()(conv_block01) # last Conv block";
 	}
 
 	@Override
